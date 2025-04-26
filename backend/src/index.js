98cbf9d1042d2e41f6cab5614a7eda373930307a
Rokaya Nasr <rokanasr60@gmail.com>
2025-04-26 21:38:28 +0300

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL2],
         credentials: true,
     })
 );
