a8ac1d3fc2d9fb932945853634fdaa61d1110d05
Rokaya Nasr <rokanasr60@gmail.com>
2025-04-29 11:36:47 +0300

    socket change

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
     cors({
-        origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL2],
+        origin: "http://localhost:5173",
         credentials: true,
     })
 );
@@ -30,11 +30,11 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
+  
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
-}
+  }
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
