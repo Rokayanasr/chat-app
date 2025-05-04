08df5a74ddb6312b5f44f246f201461bc5a109b2
Rokaya Nasr <rokanasr60@gmail.com>
2025-05-05 01:41:57 +0300

    footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
-    <footer>
-        <div className="container">
-            <p>copyright &copy; {new Date().getFullYear()} developed by <Link to="https://www.linkedin.com/in/rokayanasr/">Rokaya Nasr</Link></p>
+    <footer className=' bg-primary'>
+        <div className="container mx-auto">
+            <p className='text-white text-center py-2'>copyright &copy; {new Date().getFullYear()} developed by <Link to="https://www.linkedin.com/in/rokayanasr/" className='text-white'>Rokaya Nasr</Link></p>
         </div>
     </footer>
   )
